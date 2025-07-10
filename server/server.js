f605bc5cc742a66d1b0c7944d608406e541c739d
Divy Patel <pateldivy842005@gmail.com>
2025-07-10 22:37:20 +0530

    React frontend for E-Mart

--- server/server.js
+++ server/server.js
@@ -42,9 +42,9 @@ app.listen(PORT, () => {
       .white
   );
 });
-const cors = require("cors");
+// const cors = require("cors");
 
-app.use(cors({
-  origin: "https://sparkly-blancmange-350149.netlify.app/",  // your frontend domain
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "https://sparkly-blancmange-350149.netlify.app/",  // your frontend domain
+//   credentials: true
+// }));
